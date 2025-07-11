619c59abe0d90a88db1949587caec7a76c2849dd
Abhinay6227 <135729454+Abhinay6227@users.noreply.github.com>
2025-07-11 15:38:35 +0530

    11-07-25

--- server.js
+++ server.js
@@ -3,5 +3,5 @@
 const { createStrapi } = require('@strapi/strapi');
 
 createStrapi().start().catch((err) => {
-  console.error('❌ Strapi failed to start:', err);
+  console.error('❌ Strapi failed to start:', JSON.stringify(err, null, 2));
 });
