ff7ea1b703b203321527a23636db03c2ff6dab07
Abhinay6227 <135729454+Abhinay6227@users.noreply.github.com>
2025-07-11 15:53:40 +0530

    hello12

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ const { createStrapi } = require('@strapi/strapi');
 createStrapi().start().catch((err) => {
   console.error('❌ Strapi failed to start:', JSON.stringify(err, null, 2));
 });
+console.log('ALL ENV VARS AT STARTUP:', process.env);
