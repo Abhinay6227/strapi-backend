c34325f8c74cd4fddf3e15adfe614a16bc8a5643
Abhinay6227 <135729454+Abhinay6227@users.noreply.github.com>
2025-09-02 00:58:26 +0530

    Changed something

--- config/database.js
+++ config/database.js
@@ -1,18 +1,26 @@
-module.exports = ({ env }) => ({
-  connection: {
+// config/env/production/database.js
+
+module.exports = ({ env }) => {
+  const connection = {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
     },
     pool: {
-      min: 2,
-      max: 10,
+      min: env.int('DATABASE_POOL_MIN', 2),
+      max: env.int('DATABASE_POOL_MAX', 10),
     },
     acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-  },
-});
+  };
+
+  // Only add the SSL object if DATABASE_SSL is true
+  if (env.bool('DATABASE_SSL', false)) {
+    connection.connection.ssl = { rejectUnauthorized: false };
+  }
+
+  return connection;
+};
